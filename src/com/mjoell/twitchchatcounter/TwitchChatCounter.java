630eb54d07f8270632b5d249a64a2be2f14de6b7
mjoell <me@mjoell.com>
2014-06-08 19:48:34 -0700

    mysql arguments, start TwitchConnect

--- src/com/mjoell/twitchchatcounter/TwitchChatCounter.java
+++ src/com/mjoell/twitchchatcounter/TwitchChatCounter.java
@@ -1,5 +1,7 @@
 package com.mjoell.twitchchatcounter;
 
+import java.sql.SQLException;
+
 public class TwitchChatCounter {
 	public static String username;
 	public static String password;
@@ -8,16 +10,16 @@ public class TwitchChatCounter {
 	public static String mysqlpassword;
 	
 	public static void main(String args[]) throws Exception {
-		if(args.length != 3) {
+		if(args.length != 4) {
 			System.out.println("You messed up, please fix.");
 		} else {
 			username = args[0];
 			password = args[1];
 			channel = "#" + args[2];
+			mysqluser = args[3];
+			mysqlpassword = "";
+						
+			TwitchConnect.main();
 		}
-		
-		//TwitchConnect.main();
-		
-		Mysql.getMostActiveFromChannel("nosleeptv", "10");
 	}
 }
