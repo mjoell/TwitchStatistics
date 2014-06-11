a9e8b0c21bb1e791dd891b57a7da41d073bf95e4
Michael Joell <me@mjoell.com>
2014-06-11 00:12:39 -0700

    Joins each channel in array

--- src/com/mjoell/twitchchatcounter/TwitchConnect.java
+++ src/com/mjoell/twitchchatcounter/TwitchConnect.java
@@ -10,6 +10,14 @@ public class TwitchConnect {
 			bot.setVerbose(false);
 		}
 		bot.connect("irc.twitch.tv", 6667, TwitchChatCounter.password);
-		bot.joinChannel(TwitchChatCounter.channel);
+		System.out.println("Connected to irc.twitch.tv");
+		
+		for(int i = 0; i < TwitchChatCounter.channels.length; i++) {
+			bot.joinChannel(TwitchChatCounter.channels[i]);
+			System.out.println("Joined channel " + TwitchChatCounter.channels[i] + ".  Waiting 4 seconds before joining next.");
+			Thread.sleep(4000);
+		}
+		
+		System.out.println("Joined all channels.  Assuming you're not in verbose, this is the last you'll hear from us for a while.");
 	}
 }
